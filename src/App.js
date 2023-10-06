e899c5ce3f82038a74fb2802ad71026c6b102af2
Akshay Rk <39210915+akshayark97@users.noreply.github.com>
2023-10-06 23:11:39 +0530

    Created React todo app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import "./styles.css";
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function App() {
-  const [todos, setTodos] = useState([])
-  const [todoInput, setTodoInput] = useState('')
+  const [todos, setTodos] = useState([]);
+  const [todoInput, setTodoInput] = useState("");
 
   const addTodo = () => {
-    setTodos([...todos, todoInput])
-    setTodoInput('')
-  }
+    setTodos([...todos, todoInput]);
+    setTodoInput("");
+  };
   return (
     <div className="App">
-      <input type="text" onChange={(e) => setTodoInput(e.target.value)} placeholder="Enter Todo..."/>
+      <input
+        type="text"
+        onChange={(e) => setTodoInput(e.target.value)}
+        placeholder="Enter Todo..."
+      />
       <button onClick={addTodo}>Add Todo</button>
       <div>
-        {todos.map(todo => (
+        {todos.map((todo) => (
           <li>{todo}</li>
         ))}
       </div>
